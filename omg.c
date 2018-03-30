7dac3cbd7f27e8a1040975566b7c50e98039be5a
Dweepa <dweepa.prasad@gmail.com>
2018-03-30 19:49:40 +0530

    stuff

--- omg.c
+++ omg.c
@@ -231,197 +231,151 @@ void* intal_diff(void* intal1, void* intal2)
 }
 
 //Multiplies two intals and returns the product.
-char* rev(char* str)
-{
-    int i = 0,temp=0;
-    int j = strlen(str) - 1;
+typedef unsigned int uint32;
+
+enum PaddingType { LEFT, RIGHT };
+bool isZero(const string& oper) {
+    for (size_t i = 0, l = oper.size(); i < l; i++)
+        if (oper[i] != '0') return false;
+    return true;
+}
+char* padString(const string& toPad, size_t paddingCountToAdd, char paddingChar, PaddingType paddingType = RIGHT) {
+    string s(paddingCountToAdd, paddingChar);
+    return paddingType == LEFT ? s + toPad : toPad + s;
+}
+char* removeLeftZeros(const string& value) {
+    // Delete '0's from the left
+    if (value.size() > 1 && value[0] == '0') {
+        for (size_t i = 0, l = value.size(); i < l; i++) {
+            if (value[i] == '0')
+                continue;
+            return value.substr(i, l - i);
+        }
+    }
+    return value;
+}
+char*mulIntStringByChar(const string& op1, const char op2) {
+    if (op2 == '0') return "0";
+    if (op2 == '1') return op1;
     
-    while (i < j) {
-        temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
-        i++;
-        j--;
+    int carry = 0;
+    int d0 = op2 - '0';
+    char*r = "";
+    for (int j = op1.size() - 1; j >= 0; j--) {
+        int d1 = op1[j] - '0';
+        int d = (d0 * d1) + carry;
+        carry = d / 10;
+        d %= 10;
+        r += d + '0';
     }
-    return str;
+    if (carry) r += carry + '0';
+    reverse(r.begin(), r.end());
+    return r;
 }
-char* itoa(int i, char b[]){
-    char const digit[] = "0123456789";
-    char* p = b;
-    if(i<0){
-        *p++ = '-';
-        i *= -1;
+char* subIntStrings(const string& op1, const string& op2) {
+    if (op2 == "") return op1;
+    
+    char*total = "";
+    size_t op1Size = op1.size();
+    size_t op2Size = op2.size();
+    
+    uint32 carry = 0;
+    for (int i = 0; i < op1Size; i++) {
+        int c0 = op1[op1Size - 1 - i] - '0' - carry;
+        int c1 = i < op2Size ? op2[op2Size - 1 - i] - '0' : 0;
+        carry = 0;
+        if (c0 < c1) {
+            carry = 1;
+            c0 += 10;
+        }
+        total += (c0 - c1) + '0';
     }
-    int shifter = i;
-    do{ //Move to where representation ends
-        ++p;
-        shifter = shifter/10;
-    }while(shifter);
-    *p = '\0';
-    do{ //Move back, inserting digits as u go
-        *--p = digit[i%10];
-        i = i/10;
-    }while(i);
-    return b;
+    reverse(total.begin(), total.end());
+    return isZero(total) ? "0" : removeLeftZeros(total);
 }
+char* sumIntStrings(const string& op1, const string& op2) {
+    if (op2 == "") return op1;
+    if (op1 == "") return op2;
+    char*total = "";
+    size_t op1Size = op1.size();
+    size_t op2Size = op2.size();
+    size_t m = max(op1Size, op2Size);
+    
+    uint32 carry = 0;
+    for (int i = 0; i < static_cast<int>(m); i++) {
+        uint32 c0 = i < op1Size ? op1[op1Size - 1 - i] - '0' : 0;
+        uint32 c1 = i < op2Size ? op2[op2Size - 1 - i] - '0' : 0;
+        uint32 c = c0 + c1 + carry;
+        carry = c / 10;
+        total += (c % 10) + '0';
+    }
+    if (carry) total += carry + '0';
+    reverse(total.begin(), total.end());
+    return total;
+}
+char*multKaratsuba(const string& oper1, const string& oper2) {
+    // 2 * O(n)
+    if (isZero(oper1) || isZero(oper2)) {
+        return "0";
+    }
+    
+    // 2 * O(n)
+    char*op1 = removeLeftZeros(oper1);
+    char*op2 = removeLeftZeros(oper2);
+    
+    // O(1) (theoritically)
+    size_t l1 = op1.size();
+    size_t l2 = op2.size();
+    
+    if (l1 == 1 || l2 == 1)
+        // O(1)
+        return mulIntStringByChar(l1 == 1 ? op2 : op1, l1 == 1 ? op1[0] : op2[0]);
+    
+    // Divide
+    size_t n = max(l1, l2);
+    n += (n & 1);
+    
+    // 2 * O(n)
+    op1 = padString(op1, n - l1, '0', LEFT);
+    op2 = padString(op2, n - l2, '0', LEFT);
+    
+    size_t n2 = n / 2;
+    
+    // 4 * O(n)
+    char*a = op1.substr(0, n2);
+    char*b = op1.substr(n2, n2);
+    char*c = op2.substr(0, n2);
+    char*d = op2.substr(n2, n2);
+    
+    // 2 recursive calls
+    char*ac = multKaratsuba(a, c);
+    char*bd = multKaratsuba(b, d);
+    
+    // 2 * O(n)
+    char*_ab = sumIntStrings(a, b);
+    char*_cd = sumIntStrings(c, d);
+    
+    // another call
+    char*abcd = multKaratsuba(_ab, _cd);
+    
+    // 2 * O(n)
+    abcd = subIntStrings(abcd, ac);
+    abcd = subIntStrings(abcd, bd);
+    
+    // 5 * O(n)
+    ac = padString(ac, n, '0', RIGHT);
+    abcd = padString(abcd, n2, '0', RIGHT);
+    char*res = sumIntStrings(ac, abcd);
+    res = sumIntStrings(res, bd);
+    return removeLeftZeros(res);
+}
+
 void* intal_multiply(void* intal1, void* intal2)
 {
-    char* num1 = intal2str(intal1);
-    char* num2 = intal2str(intal2);
-    int n1=strlen(num1),n2=strlen(num2);
-    int max=0,i,j,tmp=0;
-    char a[n1],b[n2];
-    int* mul=(int*)malloc(n1+n2);
-    
-    printf("\nProduct: ");
-    char* product = (char*)malloc(n1+n2+1);
-    for(i=0;i<n1+n2;i++)
-        product[i]='0';
-    product[n1+n2+1]='\0';
-    printf("%s\n",product);
-    
-    printf("n1:%d  n2:%d  \n",n1,n2);
-    
-    for(i=n1-1,j=0;i>=0;i--,j++)
-    {
-        a[j] = num1[i]-'0';
-    }
-    for(i=n2-1,j=0;i>=0;i--,j++)
-    {
-        b[j] = num2[i]-'0';
-    }
-    for(i=0;i<n1;i++)
-    {
-        printf("a,b: %d  %d  ",a[i],b[i]);
-    }
-    printf("\nMul: ");
-    for(i=0;i<n1+n2;i++)
-    {
-        mul[i]=0;
-        printf("%d",mul[i]);
-    }
-    printf("\n\nMultiplying all the integers: \n");
-    for(i=0;i<n2;i++)
-    {
-        for(j=0;j<n1;j++)
-        {
-            printf("a:%2d b:%2d mul:%3d ==> ",a[j],b[i],mul[i+j]);
-            mul[i+j] += b[i]*a[j];
-            printf("%2d %2d %3d\n",a[j],b[i],mul[i+j]);
-        }
-    }
-    for(i=n1+n2-1;i>=0;i--)
-    {
-        printf("%d",mul[i]);
-    }
-    printf("\nTransferring the carry's:\n");
-    for(i=0;i<n1+n2;i++)
-    {
-        tmp = mul[i]/10;
-        mul[i] = mul[i]%10;
-        mul[i+1] = mul[i+1] + tmp;
-        printf("%d ",mul[i]);
-    }
-    printf("\n");
-    for(i=0;i<n1+n2;i++)
-    {
-        printf("%d ",mul[i]);
-    }
-    printf("\n");
-    //Extra zeroes in the beginning.
-    for(i=n1+n2-1;i>=0;i--)
-    {
-        if(mul[i] > 0)
-        {
-            printf("%d-%d  ",mul[i],i);
-            break;
-        }
-    }
-    max=n1+n2-1;
-    
-    j=i;
-    printf("\nP: max:%d j:%d i:%d\n",max,j,i);
-    for(j=i;j>=0;j--)
-    {
-        product[j]=mul[j]+'0';
-        printf("mul[%d]=%3d product[%d]=%3c\n",j,mul[j],j,product[j]);
-    }
-    printf("p: %s\n",product);
-    product=rev(product);
-    free(mul);
-    i=0;
-    while(product[i]=='0')
-        i++;
-    printf("%s\n",product);
-    return product+i;
-}
-char* karatsuba(char* num1,char* num2)
- {
- int n1=strlen(num1),n2=strlen(num2),max=0,i=0;
- 
- if(num1[i]-'0'<10 && num2[i]-'0'<10)
- return ((num1[i]-'0')*(num2[i]-'0'))+'0';
- 
- if(n1>n2)
- {
- max=n1;
- int1 = (char*)malloc(max+1);
- int2 = (char*)malloc(max+1);
- 
- strcpy(int1,num1);
- strcpy(int2+(n1-n2),num2);
- for(i=(n1-n2);i>0;i--)
- int2[i]='0';
- 
- int1[max]='\0';
- int2[max]='\0';
- }
- else if(n1<n2)
- {
- max=n2;
- int1 = (char*)malloc(max+1);
- int2 = (char*)malloc(max+1);
- 
- strcpy(int2,num2);
- strcpy(int1+(n2-n1),num1);
- for(i=(n2-n1);i>0;i--)
- int1[i]='0';
- 
- int1[max]='\0';
- int2[max]='\0';
- }
- free(num1);
- free(num2);
- else
- max=n2;
- int half = max/2;
- 
- 
- char* num1_low = int1.substr(0,half);
- char* num2_low = int2.substr(half,max-half);
- char* num1_high = int1.substr(0,half);
- char* num2_high = int2.substr(half,max-half);
- 
- void* a1 = intal_create(num1_low);
- void* a2 = intal_create(num1_high);
- void* b1 = intal_create(num2_low);
- void* b2 = intal_create(num2_high);
- 
- char* p0 = karatsuba(num1_low,num2_low);
- char* p1 = karatsuba(num1_high,num2_high);
- intal_add(a1,a2)
- char* p2 = karatsuba(,intal_add(b1,b2));
- char* p3 = intal_diff(intal_create(p2),intal_add(intal_create(p0),intal_create(p1)));
- 
- }
-intal *mul10pow(_intal *x, long int y) {
-    void *create_by_length(long int n);
-    _intal *ans = create_by_length((x->num_digits) + y);
-    memcpy(ans->digits, x->digits, x->num_digits);
-    int i;
-    for (i = x->num_digits; i < ans->num_digits; i++)
-        ans->digits[i] = '0';
-    return ans;
+    char* num1 = (char*) intal1;
+    char* num2 = (char*)intal2;
+    char* product = multKaratsuba(num1,num2);
+    return product;
 }
 
 //Integer division
